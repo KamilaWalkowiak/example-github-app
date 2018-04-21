318beed23f85f87795b54af814a087ffdf65ddd8
KamilaWalkowiak <kamila.bartkow@gmail.com>
2018-04-21 10:46:50 +0200

    Add styles to footer and header.

--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 import {Container} from "./home.style";
+import Header from '../../components/header/header.component';
+import Footer from '../../components/footer/footer.component';
 
 class Home extends Component {
     render() {
         return (
-            <Container>
-                home
-            </Container>
+            <div>
+                <Header/>
+                <Container>
+                    home
+                </Container>
+                <Footer/>
+            </div>
+
         );
     }
 }
