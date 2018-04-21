e497b25fdc5c2f5772dd1f4855fcfa1f49c3f97d
KamilaWalkowiak <kamila.bartkow@gmail.com>
2018-04-21 09:11:57 +0200

    Add styled components library.

---
+++ src/routes/home/home.component.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Container} from "./home.style";
+
+class Home extends Component {
+    render() {
+        return (
+            <Container>
+                home
+            </Container>
+        );
+    }
+}
+
+export default Home;
